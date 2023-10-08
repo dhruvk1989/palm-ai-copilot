298bb14c7aaeaee3fcf9a1d2668821cfdc41c02e
dhruvsinghk <dhruvk1989>
2023-10-08 07:37:56 +0530

    first commit

---
+++ src/main/java/com/dhruv/PalmAiCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmAiCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalmAiCopilotApplication.class, args);
+    }
+
+}
